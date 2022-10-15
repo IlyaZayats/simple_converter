86209412186d68097a4bb23efb6f61293310de90
Ilya <ilya@Ilyas-MacBook-Pro.local>
2022-10-15 14:03:59 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import dicom2nifti
+import dicom2nifti.settings as settings
+
+
+settings.disable_validate_slice_increment()
+
+dicom_path = input("Input path to dataset: ")
+nifti_path = input("Input path where dataset will store: ")
+
+# dicom_path = "/Users/ilya/Documents/datasets/CTAG-2/"
+# nifti_path = "/Users/ilya/Documents/niftiData/"
+# nifti_path = "/Users/ilya/Documents/testTest/"
+
+dicom_paths = [
+    os.path.join(os.getcwd(), dicom_path, x)
+    for x in os.listdir(dicom_path)
+]
+
+if (not os.path.exists(nifti_path)):
+    os.makedirs(nifti_path)
+
+for path in dicom_paths:
+    if path.rpartition('/')[2] != '.DS_Store':
+        dicom2nifti.dicom_series_to_nifti(path, nifti_path + path.rpartition('/')[2], reorient_nifti=True)
+        print("Study " + path.rpartition('/')[2] + " converted!")
+
